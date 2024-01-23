a60ce547cc8e8167421421688ec7ca1aae9f8da0
Shumieb <shumiebc@gmail.com>
2024-01-23 22:31:12 +0000

    Added pages and components

---
+++ src/Pages/AddNewBookPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddNewBookPage() {
+    return (
+        <div>AddNewBookPage</div>
+    )
+}
+
+export default AddNewBookPage
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './Pages/HomePage';
+import LayoutPage from './Pages/LayoutPage';
+import NoPage from './Pages/NoPage';
+import AboutPage from './Pages/AboutPage';
+import AddNewBookPage from './Pages/AddNewBookPage';
+import EditBookPage from './Pages/EditBookPage';
+import BooksPage from './Pages/BooksPage';
+import BookPage from './Pages/BookPage';
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LayoutPage />}>
+          <Route index element={<HomePage />} />
+          <Route path="book" element={<BooksPage />} >
+            <Route path=":bookId" element={<BookPage />} />
+            <Route path="new" element={<AddNewBookPage />} />
+            <Route path="edit" element={<EditBookPage />} />
+          </Route>
+          <Route path="about" element={<AboutPage />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
